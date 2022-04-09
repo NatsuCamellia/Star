4330ded024742510e1fd42987f6b2759937345bc
NatsuCamellia <natsucamellia@gmail.com>
2022-04-09 14:07:26 +0800

    Remove unused import

--- src/Star/MainApp.java
+++ src/Star/MainApp.java
@@ -7,8 +7,6 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
-import java.io.File;
-
 public class MainApp extends Application {
     
     private Stage primaryStage;
