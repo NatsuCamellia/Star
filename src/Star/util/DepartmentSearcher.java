abdf7cb478e26a88a91d3a8defda34769da53c73
NatsuCamellia <natsucamellia@gmail.com>
2022-04-18 21:27:14 +0800

    修復自然、社會對調問題

--- src/Star/util/DepartmentSearcher.java
+++ src/Star/util/DepartmentSearcher.java
@@ -45,36 +45,19 @@ public class DepartmentSearcher {
         String rank, fil1, fil2;
 
         // 名額
-        rank = String.format("%s / %s\n", data[2], data[3]);
-        // 檢定門檻
+        rank = String.format("%s / %s", data[2], data[3]);
+        String[] subjects;
         String[] ranks = data[4].split("\n");
-        String rank_template;
-        if (year.equals("111")) {
-            rank_template = """
-                    國文\t%s
-                    英文\t%s
-                    數學A\t%s
-                    數學B\t%s
-                    自然\t%s
-                    社會\t%s
-                    英聽\t%s""";
-            rank += String.format(rank_template, ranks[0], ranks[1], ranks[2], ranks[3], ranks[4], ranks[5], ranks[6]);
-        } else {
-            rank_template = """
-                    國文\t%s
-                    英文\t%s
-                    數學\t%s
-                    自然\t%s
-                    社會\t%s
-                    英聽\t%s""";
-            rank += String.format(rank_template, ranks[0], ranks[1], ranks[2], ranks[3], ranks[4], ranks[6]);
+
+        if (year.equals("111")) subjects = new String[] {"國文", "英文", "數學A", "數學B", "社會", "自然", "英聽"};
+        else subjects = new String[] {"國文", "英文", "數學", "社會", "自然", "英聽"};
+
+        for (int i = 0; i < subjects.length; i++) {
+            rank += String.format("\n%s\t%s", subjects[i], ranks[i]);
         }
-        
-        // 篩選比序順序
+
         String[] filters = data[5].split("\n");
-        // 第一輪篩選
         String[] fil_1 = data[7].split("\n");
-        // 名額(第一輪錄取 / 錄取)
         fil1 = data[6] + " / " + data[2];
         for (int i = 0; i < filters.length; i++) {
             fil1 += "\n" + filters[i] + "\t" + fil_1[i];
@@ -83,9 +66,7 @@ public class DepartmentSearcher {
         
         // 有第二輪篩選
         if (!data[8].equals("--")) {
-            // 名額(第二輪錄取 / 錄取)
             fil2 = data[8] + " / " + data[2];
-
             String[] fil_2 = data[9].split("\n");
             for (int i = 0; i < filters.length; i++) {
                 fil2 += "\n" + filters[i] + "\t" + fil_2[i];
